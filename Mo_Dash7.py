947be3f7b36473500c562f2acc6d36e610787acb
Payam Saeedi <76733438+payamv3@users.noreply.github.com>
2025-12-21 00:39:56 -0500

    Update Mo_Dash7.py

--- Mo_Dash7.py
+++ Mo_Dash7.py
@@ -145,7 +145,7 @@ elif st.session_state.step == 2:
         show_resell = True
         show_donate = True
     elif device == "Unlisted Model" and working == "Yes":
-        show_resell = False
+        show_resell_no_model = True
         show_donate = True
     elif device == "Unlisted Model" and working != "Yes":
         show_resell = False
@@ -155,6 +155,22 @@ elif st.session_state.step == 2:
         show_donate = False
 
     if show_resell:
+        st.markdown("**Resell:** You could earn some cash by selling your old phone.")
+        #if max_price > 0:
+            #st.markdown(f"💰 Your **{device}** can fetch up to **${max_price}** on resale!")
+        st.markdown(f"<p style='font-size: 30x;'>💰 Your {device} can fetch up to ${max_price} on resale!</p>", unsafe_allow_html=True)
+            
+        #else:
+            #st.info(f"ℹ️ Could not find resale price for {device}.")
+        st.markdown( f"**It's easy to resell, either vendor will send you a box with prepaid postage.**")
+        st.markdown(
+            f"Try the following websites to get an estimate of your smartphone's current worth:  \n"
+            f'- [BackMarket](https://www.backmarket.com/en-us/buyback/home) -  This link leads to site to get quote to sell your smartphone to BackMarket \n'
+            f'- [Gazelle](https://www.gazelle.com/trade-in?_gl=1*1qgg1ts*_gcl_aw*R0NMLjE3NTc3MDA4NDguQ2p3S0NBandpWV9HQmhCRUVpd0FGYWdodnJrRElUenlqZ3M1QkU5YmJRd2JtTFRFNkxSNWc0SkJCdDhleXJXakU3emFPOXlMV2VHN01Sb0MxSThRQXZEX0J3RQ..*_gcl_au*NTk2NzI0NDQ3LjE3NTc3MDA4MzQuMzAwODg2NTE0LjE3NTgyMzExMjEuMTc1ODIzMTEyMQ..*_ga*MTU5NTIxODU5Mi4xNzQ1OTUxMjYw*_ga_6918GRRZ0Y*czE3NjM2NjE0MDIkbzYkZzEkdDE3NjM2NjE0MDQkajU3JGwwJGgxMTc4NzE4Mzg0) - This link leads to site to get quote to sell your smartphone to Gazelle \n'
+        )
+        st.markdown("Upon receiving the phone, the vendor will check battery condition, if it turns on, and if data has been wiped. If there are issues, they will likely adjust the offered price")
+        
+    if show_resell_no_model:
         st.markdown("**Resell:** You could earn some cash by selling your old phone.")
         if max_price > 0:
             #st.markdown(f"💰 Your **{device}** can fetch up to **${max_price}** on resale!")
@@ -169,8 +185,7 @@ elif st.session_state.step == 2:
             f'- [Gazelle](https://www.gazelle.com/trade-in?_gl=1*1qgg1ts*_gcl_aw*R0NMLjE3NTc3MDA4NDguQ2p3S0NBandpWV9HQmhCRUVpd0FGYWdodnJrRElUenlqZ3M1QkU5YmJRd2JtTFRFNkxSNWc0SkJCdDhleXJXakU3emFPOXlMV2VHN01Sb0MxSThRQXZEX0J3RQ..*_gcl_au*NTk2NzI0NDQ3LjE3NTc3MDA4MzQuMzAwODg2NTE0LjE3NTgyMzExMjEuMTc1ODIzMTEyMQ..*_ga*MTU5NTIxODU5Mi4xNzQ1OTUxMjYw*_ga_6918GRRZ0Y*czE3NjM2NjE0MDIkbzYkZzEkdDE3NjM2NjE0MDQkajU3JGwwJGgxMTc4NzE4Mzg0) - This link leads to site to get quote to sell your smartphone to Gazelle \n'
         )
         st.markdown("Upon receiving the phone, the vendor will check battery condition, if it turns on, and if data has been wiped. If there are issues, they will likely adjust the offered price")
-        
-
+    
     if show_donate:
         st.markdown(
             f"**Donate:** Your used phone may not fetch a high price, but if still working and holding a charge, donating gives it a new life. "
